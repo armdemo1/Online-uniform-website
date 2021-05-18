e9ea366136690d526ceb7ae6772bb609ece6e081
armdemo1 <71333509+armdemo1@users.noreply.github.com>
2021-05-18 14:39:51 +0700

    Add files via upload

---
+++ Article.php
@@ -0,0 +1,34 @@
+<?php include('h.php'); ?>
+
+<body>
+
+    <div class="container">
+        <img src="banner4.png" class="img-fluid" alt="Responsive image">
+        <?php include('navbar.php'); ?>
+        <p align="center"></p>
+        <div class="card">
+            <div class="card-body">
+                <div align="center">This website is for customers who want to buy government uniforms. </div>
+                <p align="center"></p>
+                <div align="center">They want to buy an online and can be trusted.If the store does something wrong. </div>
+                <p align="center"></p>
+                <div align="center">we apologize for that and thank customers for trusting us. </div>
+            </div>
+        </div>
+        &nbsp;
+        <div class="card">
+            <div class="card-body">
+                <div align="center">Contact: Uniform555@email.com </div>
+                <p align="center"></p>
+                <div align="center">LINE ID:uniformshop156 </div>
+                <p align="center"></p>
+                <div align="center">Phone Number:02xxxxxxx</div>
+                <p align="center"></p>
+                <div align="center">Address: In Bangkok</div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
